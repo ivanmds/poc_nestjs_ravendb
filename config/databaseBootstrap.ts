d1f185ed1f449d6d7420d4c900ff9554ad8759aa
Ivan Meireles Da Silva <isilva@acesso.com>
2020-07-07 23:29:47 -0300

    bootstrap do bando de dados

---
+++ config/databaseBootstrap.ts
@@ -0,0 +1,22 @@
+import { DocumentStore, GetDatabaseNamesOperation, CreateDatabaseOperation } from "ravendb";
+import { environment } from "./environment";
+
+export class DatabaseBootstrap {
+
+    protected readonly store: DocumentStore;
+    constructor() {
+        this.store = new DocumentStore(environment.ravendb.connectionString, environment.ravendb.databaseName);
+
+        this.store.initialize();
+    }
+
+    async initDb(): Promise<any> {
+        var getDatabaseNames = new GetDatabaseNamesOperation(0, 25);
+        const databaseNames = await this.store.maintenance.server.send(getDatabaseNames);
+
+        if (databaseNames.find(name => name == environment.ravendb.databaseName)) {
+            const createDatabase = new CreateDatabaseOperation({ databaseName: environment.ravendb.databaseName });
+            await this.store.maintenance.server.send(createDatabase);
+        }
+    }
+}
