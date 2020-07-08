87bb6d5e0d34da8fcba02fa9173ae03290bb14a0
Ivan Meireles Da Silva <isilva@acesso.com>
2020-07-08 01:59:59 -0300

    completando controller

--- src/device/device.controller.ts
+++ src/device/device.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, BadRequestException, Get, Param, NotFoundException } from "@nestjs/common";
 import { Device } from "./device.interface";
 import { DeviceService } from "./device.service";
 
@@ -7,9 +7,22 @@ export class DeviceController {
 
     constructor(private readonly deviceService: DeviceService) { }
 
+    @Get("/company/:companyKey/document/:documentNumber")
+    async Get(@Param("companyKey") companyKey: string, @Param("documentNumber") documentNumber: string) {
+        var devices = await this.deviceService.find(companyKey, documentNumber);
+
+        if(devices?.length > 0) return devices;
+        throw new NotFoundException("Devices not found");
+    }
+
     @Post()
     async Post(@Body() device: Device) {
-        await this.deviceService.save(device);
+        const result = await this.deviceService.save(device);
+        if(result.isSuccess) {
+            return result.data;
+        } else {
+            throw new BadRequestException(result.errors);
+        }
     }
 
 }
