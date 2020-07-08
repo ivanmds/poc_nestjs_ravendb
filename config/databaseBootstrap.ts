f3e13ca096ca70b3f9bba3277fe200f00fd7a64b
Ivan Meireles Da Silva <isilva@acesso.com>
2020-07-07 23:49:38 -0300

    corrigindo logica

--- config/databaseBootstrap.ts
+++ config/databaseBootstrap.ts
@@ -14,7 +14,7 @@ export class DatabaseBootstrap {
         var getDatabaseNames = new GetDatabaseNamesOperation(0, 25);
         const databaseNames = await this.store.maintenance.server.send(getDatabaseNames);
 
-        if (databaseNames.find(name => name == environment.ravendb.databaseName)) {
+        if (!databaseNames.find(name => name == environment.ravendb.databaseName)) {
             const createDatabase = new CreateDatabaseOperation({ databaseName: environment.ravendb.databaseName });
             await this.store.maintenance.server.send(createDatabase);
         }
